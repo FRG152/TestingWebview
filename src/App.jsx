66c20c4ea421ad6546062aff540d5253d0bcee58
FRG152 <romanflavio3@hotmail.com>
2024-12-30 14:47:50 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,32 @@
 import React, { useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <div>
+      <h2>Home Page</h2>
+      <p>This is the home page of the app.</p>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About Page</h2>
+      <p>This is the about page of the app.</p>
+    </div>
+  );
+}
+
+function Contact() {
+  return (
+    <div>
+      <h2>Contact Page</h2>
+      <p>This is the contact page of the app.</p>
+    </div>
+  );
+}
 
 function App() {
   useEffect(() => {
@@ -17,9 +45,29 @@ function App() {
   }, []);
 
   return (
-    <div>
-      <h1>My React Web App</h1>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
